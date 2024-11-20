a4683e3a3f17cedf6f9af513ca87f484f18fa04e
Seier <seier.bernal@gmail.com>
2024-11-19 20:11:37 -0600

    Se agrega lógica js de botón limpiar

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 let cantidad = document.getElementById('cantidad');
 let botonGenerar = document.getElementById('generar');
 let botonLimpiar = document.getElementById('limpiar');
+let contraseña = document.getElementById('contrasena');
+
+function generar (){
+
+    let numeroDigitado = parseInt (cantidad.value);
+
+    if (numeroDigitado <8){
+        alert("La cantidad de caracteres tiene que ser mayor que 8");
+
+    }
+}
+
+function limpiar(){
+    contraseña.value = '';
+    cantidad.value = '';
+}
+
+botonLimpiar.addEventListener('click', limpiar);
